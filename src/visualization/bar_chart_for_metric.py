8f997d51c77b8041b65af02a94ff620977e195b6
Damir <70011787+SpeedFireF@users.noreply.github.com>
2023-11-05 04:57:05 +0300

    Update bar_chart_for_metric.py
    Add path and name parameters

--- src/visualization/bar_chart_for_metric.py
+++ src/visualization/bar_chart_for_metric.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def build_chart(data, path='/Users/damirabdulaev/Desktop/Hypothesis2.png'):
+def build_chart(data, name, path):
     # X-axis labels (you can customize them)
     labels = ['Toxicity', 'Similarity', 'Fluency', 'J metric']
 
@@ -12,7 +12,7 @@ def build_chart(data, path='/Users/damirabdulaev/Desktop/Hypothesis2.png'):
     # Add labels and a title
     plt.xlabel('Metrics')
     plt.ylabel('Metric values')
-    plt.title('Hypothesis 2')
+    plt.title(name)
 
     # Save the chart as a PNG file
     plt.savefig(path)
